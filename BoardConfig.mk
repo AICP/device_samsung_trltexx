48f0c41108912cf0913ba193d6631929bfbf5573
jaapp <github@pieroen.nl>
2014-12-20 13:26:54 +0000

    CFLAGS_MODULE=-fno-pic
    Change-Id: If24f2537144650eb6cc5ab7957187e22da0e6c69

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,7 +31,7 @@ BOARD_RIL_CLASS := ../../../device/samsung/trltexx/ril
 
 # Modem
 MHI_MODULE:
-	make -C hardware/qcom/mhi/ -Wno-error KERNEL_DIR=$(KERNEL_OUT) KLIB=$(KERNEL_OUT) KLIB_BUILD=$(KERNEL_OUT) ARCH=$(TARGET_ARCH) $(ARM_CROSS_COMPILE)
+	make -C hardware/qcom/mhi/ CFLAGS_MODULE=-fno-pic KERNEL_DIR=$(KERNEL_OUT) KLIB=$(KERNEL_OUT) KLIB_BUILD=$(KERNEL_OUT) ARCH=$(TARGET_ARCH) $(ARM_CROSS_COMPILE)
 	mv hardware/qcom/mhi/mhi.ko $(KERNEL_MODULES_OUT)
 TARGET_KERNEL_MODULES := MHI_MODULE
 
